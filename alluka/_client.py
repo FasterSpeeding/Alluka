dd7ab03c039d661478e363b9e93a828c03dd2754
Faster Speeding <luke@lmbyrne.dev>
2022-02-16 06:10:18 +0000

    Formatting fix

--- alluka/_client.py
+++ alluka/_client.py
@@ -181,12 +181,11 @@ class Client(abc.Client):
         # <<inherited docstring from alluka.abc.Client>>.
         if descriptors := self._build_descriptors(callback):
             # Pyright currently doesn't support `is` for narrowing tuple types like this
-            kwargs.update(
-                [
-                    (n, v[1].resolve(ctx) if v[0] == _types.InjectedTypes.TYPE else await v[1].resolve_async(ctx))
-                    for n, v in descriptors.items()
-                ]
-            )
+            new_kwargs = [
+                (n, v[1].resolve(ctx) if v[0] == _types.InjectedTypes.TYPE else await v[1].resolve_async(ctx))
+                for n, v in descriptors.items()
+            ]
+            kwargs.update(new_kwargs)
 
         result = callback(*args, **kwargs)
         if asyncio.iscoroutine(result):
