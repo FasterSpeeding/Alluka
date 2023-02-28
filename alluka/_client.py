0e769b40267fe97cf5c372bd6f3ec9cb698dd7e8
Lucina <lucina@lmbyrne.dev>
2023-02-28 20:05:42 +0000

    Update _client.py

--- alluka/_client.py
+++ alluka/_client.py
@@ -41,7 +41,7 @@ from collections import abc as collections
 from . import _errors  # pyright: ignore [ reportPrivateUsage ]
 from . import _self_injecting  # pyright: ignore [ reportPrivateUsage ]
 from . import _types  # pyright: ignore [ reportPrivateUsage ]
-from . import _visitor  # pyright: ignore [ reportPrivateUsage ]
+from . import _visitor
 from . import abc as alluka
 
 if typing.TYPE_CHECKING:
