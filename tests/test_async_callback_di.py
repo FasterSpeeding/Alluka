6b83c5bcb267280a710aabc398421858e2c27a17
FasterSpeeding <lucina@lmbyrne.dev>
2024-08-11 15:00:54 +0100

    Update test_async_callback_di.py

--- tests/test_async_callback_di.py
+++ tests/test_async_callback_di.py
@@ -133,7 +133,7 @@ async def test_call_with_async_di_with_type_dependency_and_callback(context: all
     async def callback(
         value_1: int,
         value_2: str,
-        value_3: str = alluka.inject(type=MockType),  # type: ignore
+        value_3: MockType = alluka.inject(type=MockType), 
         value_4: int = alluka.inject(type=MockOtherType),
         value_5: typing.Any = alluka.inject(callback=mock_callback),
     ) -> str:
@@ -160,7 +160,7 @@ async def test_call_with_async_di_with_type_dependency(context: alluka.Context):
     async def callback(
         value_1: int,
         value_2: str,
-        value_3: str = alluka.inject(type=MockType),  # type: ignore
+        value_3: MockType = alluka.inject(type=MockType),
         value_4: int = alluka.inject(type=MockOtherType),
     ) -> str:
         assert value_1 == 69
@@ -237,7 +237,7 @@ async def test_call_with_async_di_with_type_dependency_not_found(context: alluka
     async def callback(
         _: int,
         value: str,
-        __: str = alluka.inject(type=MockType),  # type: ignore
+        __: MockType = alluka.inject(type=MockType),
         ___: int = alluka.inject(type=MockOtherType),
     ) -> str:
         raise NotImplementedError
@@ -390,7 +390,7 @@ async def test_call_with_async_di_with_defaulting_union_type_dependency(context:
     async def callback(
         value_1: int,
         value_2: str,
-        cope: int = alluka.inject(type=typing.Union[MockType, MockOtherType, None]),  # type: ignore
+        cope: typing.Union[MockType, MockOtherType, None] = alluka.inject(type=typing.Union[MockType, MockOtherType, None]),
     ) -> float:
         assert value_1 == 123
         assert value_2 == "ok"
@@ -1363,7 +1363,7 @@ async def test_call_with_async_di_with_overridden_annotated_sub_sync_dependency(
 @pytest.mark.anyio
 async def test_call_with_async_di_with_positional_only_type_dependency(context: alluka.Context):
     async def callback(
-        _: int, __: str = alluka.inject(type=float), /, ___: float = alluka.inject(type=float)  # type: ignore
+        _: int, __: float = alluka.inject(type=float), /, ___: float = alluka.inject(type=float)x
     ) -> None:
         raise NotImplementedError
 
