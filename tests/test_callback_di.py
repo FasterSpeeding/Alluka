83a66108071c57a3de0e173bbca04e31893c7bf5
FasterSpeeding <lucina@lmbyrne.dev>
2024-08-11 15:04:14 +0100

    Update test_callback_di.py

--- tests/test_callback_di.py
+++ tests/test_callback_di.py
@@ -133,7 +133,7 @@ def test_call_with_di_with_type_dependency_and_callback(context: alluka.Context)
     def callback(
         value_1: int,
         value_2: str,
-        value_3: str = alluka.inject(type=MockType),  # type: ignore
+        value_3: MockType = alluka.inject(type=MockType),
         value_4: int = alluka.inject(type=MockOtherType),
         value_5: typing.Any = alluka.inject(callback=mock_callback),
     ) -> str:
@@ -161,7 +161,7 @@ def test_call_with_di_with_type_dependency(context: alluka.Context):
     def callback(
         value_1: int,
         value_2: str,
-        value_3: str = alluka.inject(type=MockType),  # type: ignore
+        value_3: MockType = alluka.inject(type=MockType),
         value_4: int = alluka.inject(type=MockOtherType),
     ) -> str:
         assert value_1 == 69
@@ -241,7 +241,7 @@ def test_call_with_di_with_type_dependency_not_found(context: alluka.Context):
     def callback(
         _: int,
         value_1: str,
-        __: str = alluka.inject(type=MockType),  # type: ignore
+        __: MockType = alluka.inject(type=MockType),
         ___: int = alluka.inject(type=MockOtherType),
     ) -> str:
         raise NotImplementedError
@@ -383,7 +383,7 @@ def test_call_with_di_with_defaulting_union_type_dependency(context: alluka.Cont
     def callback(
         value_1: int,
         value_2: str,
-        cope: int = alluka.inject(type=typing.Union[MockType, MockOtherType, None]),  # type: ignore
+        cope: typing.Union[MockType, MockOtherType, None] = alluka.inject(type=typing.Union[MockType, MockOtherType, None]),
     ) -> float:
         assert value_1 == 123
         assert value_2 == "ok"
@@ -1249,7 +1249,7 @@ def test_call_with_di_with_overridden_annotated_sub_async_dependency(context: al
 
 def test_call_with_di_with_positional_only_type_dependency(context: alluka.Context):
     def callback(
-        _: int, __: str = alluka.inject(type=float), /, ___: float = alluka.inject(type=float)  # type: ignore
+        _: int, __: float = alluka.inject(type=float), /, ___: float = alluka.inject(type=float)
     ) -> None:
         raise NotImplementedError
 
@@ -1285,7 +1285,7 @@ def test_call_with_di_with_sub_positional_only_callback_dependency(context: allu
 
 
 def test_call_with_di_with_sub_positional_only_type_dependency(context: alluka.Context):
-    def dependency(_: str = alluka.inject(type=int), /) -> str:  # type: ignore
+    def dependency(_: str = alluka.inject(type=str), /) -> str:
         raise NotImplementedError
 
     def callback(
